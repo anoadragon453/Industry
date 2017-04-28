41bdd230a109a29cbd157d68edbd9b72900aac17
JavierCenteno <jacenve@telefonica.net>
2017-04-28 13:15:05 +0200

    .

--- src/classes/Resource.java
+++ src/classes/Resource.java
@@ -1,13 +1,7 @@
 package classes;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
@@ -132,7 +126,7 @@ public class Resource implements Nameable {
 	}
 	
 	@Override public int hashCode(){
-		return this.getType().hashCode();
+		return this.getType().hashCode() >>> 24 ^ this.getType().hashCode() >>> 16 ^ this.getType().hashCode() >>> 8 ^ this.getType().hashCode();
 	}
 	
 	@Override public boolean equals(Object object) {
