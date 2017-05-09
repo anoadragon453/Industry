6c534a56ccea8a38c724fc3eea0a71c79c5bc269
JavierCenteno <jacenve@telefonica.net>
2017-05-09 17:08:58 +0200

    Primitive amounts supported

--- src/types/Amount.java
+++ src/types/Amount.java
@@ -3,7 +3,7 @@ package types;
 import java.io.Serializable;
 
 /**
- * This class represents a certain amount of a certain resource as an (int, Resource) pair.
+ * This class represents a certain quantity of a certain resource as a value to represent each of them. It also offers methods to represent these objects as a primitive to avoid object overhead.
  * @author Javier
  *
  */
@@ -16,23 +16,23 @@ public class Amount implements Serializable {
 	// Class fields --------------------------------
 	
 	/**
-	 * Quantity of units of this resource this amount contains.
-	 */
-	short quantity;
-	/**
 	 * Index of the resource within the list.
 	 */
 	short index;
+	/**
+	 * Quantity of units of this resource this amount contains.
+	 */
+	short quantity;
 	
-	// Constructor --------------------------------
+	// Constructors --------------------------------
 	
 	/**
 	 * Creates a new, empty amount of the resource with the specified index in the global list of resources.
 	 * @param index
 	 */
 	public Amount(short index) {
-		this.quantity = 0;
 		this.index = index;
+		this.quantity = 0;
 	}
 	
 	/**
@@ -40,12 +40,22 @@ public class Amount implements Serializable {
 	 * @param resource
 	 */
 	public Amount(Resource resource) {
-		this.quantity = 0;
 		int index = Resource.naturalResources.indexOf(resource);
 		this.index = (short) index;
+		this.quantity = 0;
 	}
 	
-	// Methods --------------------------------
+	// Primitive constructors --------------------------------
+	
+	public static int newAmount(short index) {
+		return index << 16;
+	}
+	
+	public static int newAmount(Resource resource) {
+		return Resource.naturalResources.indexOf(resource) << 16;
+	}
+	
+	// Getters --------------------------------
 	
 	public int getQuantity() {
 		return quantity;
@@ -58,6 +68,22 @@ public class Amount implements Serializable {
 	public Resource getResource() {
 		return Resource.naturalResources.get(index);
 	}
+	
+	// Primitive getters --------------------------------
+	
+	public static int getQuantity(int amount) {
+		return (short)amount;
+	}
+	
+	public static int getIndex(int amount) {
+		return amount >>> 16;
+	}
+	
+	public static Resource getResource(int amount) {
+		return Resource.naturalResources.get(amount >>> 16);
+	}
+	
+	// Methods --------------------------------
 	
 	/**
 	 * Attempts to retrieve a certain amount of resource from this Amount and returns a new Amount with the retrieved amount of the resource.
@@ -108,6 +134,58 @@ public class Amount implements Serializable {
 					amount.quantity = 0;
 				}
 			}
+			return true;
+		}
+		return false;
+	}
+	
+	// Primitive methods --------------------------------
+	
+	/**
+	 */
+	public static int fork(int[] from, int fromIndex, short quantity) {
+		if((short)from[fromIndex] <= quantity) {
+			int ret = from[fromIndex];
+			from[fromIndex] &= 0xFFFF0000;
+			return ret;
+		} else {
+			from[fromIndex] -= quantity;
+			return (from[fromIndex] & 0xFFFF0000) | quantity;
+		}
+	}
+	
+	/**
+	 */
+	public static boolean pile(int[] from, int fromIndex, int[] to, int toIndex, short quantity) {
+		short indexFrom = (short) (from[fromIndex] >>> 16);
+		short quantityFrom = (short)from[fromIndex];
+		short indexTo = (short) (to[toIndex] >>> 16);
+		short quantityTo = (short)to[toIndex];
+		if(indexTo == indexFrom) {
+			quantityTo += quantity;
+			quantityFrom -= quantity;
+			//If quantity results to be less than 0, we move amounts back so both are non negative.
+			if(quantityTo < 0) {
+				if(quantity > 0) {//If positive + positive = negative, there's been an overflow
+					quantityFrom = (short) (quantityFrom + quantityTo - 0x7FFF);
+					quantityTo = 0x7FFF;//Maximum positive value for signed short, change if the type of primitive changes.
+				} else {//If there isn't an overflow, we just return the amount necessary for neither to be negative
+					quantityFrom = (short) (quantityFrom + quantityTo);
+					quantityTo = 0;
+				}
+			}
+			//If quantity results to be less than 0, we move amounts back so both are non negative.
+			if(quantityFrom < 0) {
+				if(quantity < 0) {//If positive + positive = negative, there's been an overflow
+					quantityTo = (short) (quantityTo + quantityFrom - 0x7FFF);
+					quantityFrom = 0x7FFF;//Maximum positive value for signed short, change if the type of primitive changes.
+				} else {//If there isn't an overflow, we just return the amount necessary for neither to be negative
+					quantityTo = (short) (quantityTo + quantityFrom);
+					quantityFrom = 0;
+				}
+			}
+			from[fromIndex] = (indexFrom << 16) | quantityFrom;
+			to[toIndex] = (indexTo << 16) | quantityTo;
 			return true;
 		}
 		return false;
