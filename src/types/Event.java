df4d0cd3c942f018256dadef898922a990d13bdf
JavierCenteno <jacenve@telefonica.net>
2017-05-16 10:20:40 +0200

    Type structure applied to Event

--- src/types/Event.java
+++ src/types/Event.java
@@ -1,11 +1,8 @@
 package types;
 
 import java.io.Externalizable;
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
 
-public class Event implements Externalizable {
+public abstract class Event implements Nameable, Externalizable {
 	
 	// Static fields --------------------------------
 	
@@ -23,18 +20,11 @@ public class Event implements Externalizable {
 	public Event(int date) {
 		this.date = date;
 	}
-
-	@Override
-	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void writeExternal(ObjectOutput out) throws IOException {
-		// TODO Auto-generated method stub
-		
+	
+	// Methods --------------------------------
+	
+	public String getType() {
+		return type;
 	}
 	
-	//TODO: CONSIDER MAKING THIS CLASS ABSTRACT AND MAKING SUBCLASSES FOR EACH TYPE OF EVENT?
 }
