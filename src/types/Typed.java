c1129d960ffee5972e44011ee01caa8885d30cbd
JavierCenteno <jacenve@telefonica.net>
2017-05-18 03:14:14 +0200

    .

--- src/types/Typed.java
+++ src/types/Typed.java
@@ -36,17 +36,12 @@ public @interface Typed {
 	/**
 	 * Enumeration of the relationships a @Typed class may have with the types of its instances.
 	 * @see types.Typed
-	 * @see types.Typed.Typification.CLASS
-	 * @see types.Typed.Typification.INSTANCE
-	 * @see types.Typed.Typification.FIELD
-	 * @see types.Typed.Typification.GLOBAL
 	 * @author Javier
 	 *
 	 */
 	public static enum Typification {
 		/**
 		 * One class, one type.
-		 * 
 		 * The type of the instances of this class is determined by which class was instanced.
 		 * These are typically implemented by extending this class and declaring new properties in every class.
 		 * The type is checked by checking the class.
@@ -54,7 +49,6 @@ public @interface Typed {
 		CLASS,
 		/**
 		 * One instance, one type.
-		 * 
 		 * Each instance of this class defines a type in itself and its properties.
 		 * Instances of other classes refer to the instances of this class to define what type of thing they refer to.
 		 * The type is checked by checking the reference.
@@ -62,7 +56,6 @@ public @interface Typed {
 		INSTANCE,
 		/**
 		 * Multiple instances, one type.
-		 * 
 		 * The type or properties of each instance of this class is stored in its fields.
 		 * Different instances of the same class may share a type by defining the same type-determining properties or characteristics within, or be of different types.
 		 * The type is checked by checking the contents of a field.
@@ -70,7 +63,6 @@ public @interface Typed {
 		FIELD,
 		/**
 		 * All instances, one type.
-		 * 
 		 * All instances of this class are considered to be of a single type.
 		 * The type is determined by the class.
 		 * The type doesn't need to be checked.
